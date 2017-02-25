0aecb61afbb198907c7238a7a032d716a2cdc0d8
MikeMangione <mjbm@bu.edu>
2017-02-25 11:24:03 -0500

    improved computer turns

--- nim_game.py
+++ nim_game.py
@@ -13,12 +13,6 @@ def print_rules():
     time.sleep(0.1)
     print '\n'
     time.sleep(0.1)
-    print '\n'
-    time.sleep(0.1)
-    print '\n'
-    time.sleep(0.1)
-    print '\n'
-    time.sleep(0.1)
     print '   ▄██████▄     ▄████████   ▄▄▄▄███▄▄▄▄      ▄████████      '
     time.sleep(0.1)
     print '  ███    ███   ███    ███ ▄██▀▀▀███▀▀▀██▄   ███    ███      '
@@ -71,7 +65,7 @@ def print_rules():
     time.sleep(0.1)
     print ' ▀█   █▀  █▀    ▀█   ███   █▀           '
     time.sleep(0.5)
-    print("\nThis is a terrible NIM player\n")
+    print("\nThis is a NIM player\n")
     time.sleep(0.5)
     print("The objective is to remove the last chip from the board\n")
     time.sleep(0.5)
@@ -79,12 +73,6 @@ def print_rules():
     time.sleep(0.5)
     print("The game ends when all the chips are gone!\n")
     time.sleep(0.5)
-    print '\n'
-    time.sleep(0.1)
-    print '\n'
-    time.sleep(0.1)
-    print '\n'
-    time.sleep(0.1)
     print '\n'
     time.sleep(0.1)
 
@@ -111,7 +99,8 @@ def r_quit_routine():
     time.sleep(2)
     return True
 
-def print_table(t):
+def print_table(t,b_x):
+    print "\nTable XOR Value:",bin(b_x),b_x
     print "\n#--------------------------#\n"
     for x in range(0,len(t)):
         print 'Column '+str(x)+':','0' * t[x],'('+str(t[x])+')'
@@ -123,7 +112,7 @@ def calc_table_xor(t):
         b_x = b_x ^ t[x]
     return b_x
 
-def compy_turn(t,b_x):
+def compy_turn(t):
     mod_temp = -1
     temp = list(set(t))
     for x in temp:
@@ -138,15 +127,20 @@ def compy_turn(t,b_x):
             mod_temp = temp[x]
             temp[x] = temp_xor
             break
+        elif temp_xor == temp[x] and temp_xor != 0:
+            mod_temp = temp[x]
+            temp[x] = 0
+            break
     if mod_temp != -1:
+        sub = t[t.index(mod_temp)] - temp[x]
         t[t.index(mod_temp)] = temp[x]
     else:
         for x in range(0,len(t)):
             if t[x] != 0:
                 t[x] += -1
-                temp_xor = t[x]
+                sub = 1
                 break
-    return t, x, temp_xor
+    return t, x, sub
 
 def player_turn(table,quit):
     col = -1
@@ -173,52 +167,82 @@ def player_turn(table,quit):
                 count = 0
     return col, count, quit
 
-def play_nim(c_v_C):
+def play_nim(C_V_C,player_number):
     quit, loss = False, False
     cols = random.randint(2,10)
     table = []
     for x in range(0,cols):
         table.append(random.randint(1,15))
-    print_table(table)
+    print_table(table,calc_table_xor(table))
     while sum(table) > 0:
-        bit_xor = calc_table_xor(table)
-        print "\nTable XOR Value:",bin(bit_xor),bit_xor
-        if c_v_c:
-            table, col, count = compy_turn(table,bit_xor)
-            print "\nComputer 1 changed column",col,"to",count
+        if C_V_C:
+            table, col, count = compy_turn(table)
+            print "\nComputer 1 removed",count,"from column",col
+            print_table(table,calc_table_xor(table))
+            time.sleep(1)
+            if sum(table) == 0:
+                break
         else:
-            col, count, quit = player_turn(table,quit)
-            table[int(col)] += -1 * int(count)
-        if quit:
-            break
-        print_table(table)
-        if sum(table) == 0:
-            break
+            if player_number == 1:
+                col, count, quit = player_turn(table,quit)
+                table[int(col)] += -1 * int(count)
+                if quit:
+                    break
+                if sum(table) == 0:
+                    break
+            else:
+                table, rm_column, rm_count = compy_turn(table)
+                print "\nThe computer removed",rm_count,"from column",rm_column
+                print_table(table,calc_table_xor(table))
+                if sum(table) == 0:
+                    loss = True
+                    break
+
         bit_xor = calc_table_xor(table)
-        print "\nTable XOR Value:",bin(bit_xor),bit_xor
-        table, rm_column, rm_count = compy_turn(table,bit_xor)
-        if c_v_c:
-            print "\nComputer 2 changed column",rm_column,"to",rm_count
+        if C_V_C:
+            table, rm_column, rm_count = compy_turn(table)
+            print "\nComputer 2 removed",rm_count,"from column",rm_column
+            print_table(table,calc_table_xor(table))
+            time.sleep(1)
+            if sum(table) == 0:
+                loss = True
+                break
         else:
-            print "\nThe computer changed column",rm_column,"to",rm_count
-        print_table(table)
-        if sum(table) == 0:
-            loss = True
-            break
-    if loss and not c_v_c:
+            if player_number == 2:
+                col, count, quit = player_turn(table,quit)
+                table[int(col)] += -1 * int(count)
+                if quit:
+                    break
+                if sum(table) == 0:
+                    break
+            else:
+                table, rm_column, rm_count = compy_turn(table)
+                print "\nThe computer removed",rm_count,"from column",rm_column
+                print_table(table)
+                if sum(table) == 0:
+                    loss = True
+                    break
+
+    if (loss or quit) and not C_V_C:
         print '\nOh no! You lost. There\'s always next time.'
         time.sleep(0.1)
         print '(like you\'ll do better)'
-    elif not c_v_c:
+    elif not C_V_C:
         print "\nCool, you won!"
     elif loss:
-        print "\n Computer Player 2 wins."
+        print "\nComputer Player 2 wins."
     else:
-        print "\n Computer Player 1 wins."
+        print "\nComputer Player 1 wins."
 
 print_rules()
 desire_to_play = y_n_answer(raw_input("\nWould you like to play? (y = yes) "))
 while desire_to_play:
     c_v_c = y_n_answer(raw_input("\nComputer v. Computer? "))
-    play_nim(c_v_c)
+    if c_v_c:
+        play_nim(c_v_c,1)
+    else:
+        id =  0
+        while id != 1 and id != 2:
+            id = int(raw_input("\nPlayer (1) or Player (2)? "))
+        play_nim(c_v_c,id)
     desire_to_play = y_n_answer(raw_input("\nWould you like to play again? "))
